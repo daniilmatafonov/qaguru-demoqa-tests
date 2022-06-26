12a91c5fa11bb826013a9288978815694bc4888a
Daniil Matafonov <danielmt.dm@gmail.com>
2022-06-26 14:02:51 +0300

    init tests. add url for automation-practice-form

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selene.support.shared import browser
+
+url = 'https://demoqa.com'
+name = 'chrome'
+width = '1360'
+height = '768'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def config():
+    browser.config.base_url = url
+    browser.config.browser_name=name
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+
+@pytest.fixture
+def init():
+    browser.open(url)
---
+++ tests/test_submit_form.py
@@ -0,0 +1,5 @@
+from selene.support.shared import browser
+
+
+def test_submit_form(init):
+    browser.open('/automation-practice-form')
